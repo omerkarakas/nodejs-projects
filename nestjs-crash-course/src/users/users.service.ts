fa7f98d31292d1a315e33a6075cf61e1e9dcbb97
Omer Karakas <karakasomer@gmail.com>
2022-07-03 23:23:35 +0300

    nestjs related things

---
+++ nestjs-crash-course/src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [{ id: 0, name: 'omer' }];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findById(userId: number): User {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  createUser(createUserDto: CreateUserDto): User {
+    const user = { id: Date.now(), ...createUserDto };
+    this.users.push(user);
+    return user;
+  }
+}
