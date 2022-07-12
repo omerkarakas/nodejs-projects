ab527bfa641b304f7d96d1ef7cdff5227a074239
Omer Karakas <karakasomer@gmail.com>
2022-07-12 23:01:22 +0300

    file sharing app added

---
+++ file-sharing/server.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const multer = require('multer');
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const File = require('./models/File');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+//middleware function
+const upload = multer({ dest: 'uploads' });
+//files copied to "uploads" directory
+
+mongoose.connect(process.env.DATABASE_URL);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+  if (req.body.password !== null && req.body.password !== '') {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+  //console.log(file);
+  //res.send(file.originalName);
+  res.render('index', { fileLink: `${req.headers.origin}/file/${file.id}` });
+});
+
+app.get('/file/:id', handleDownload);
+app.post('/file/:id', handleDownload);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render('password');
+      return;
+    }
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render('password', { error: true });
+      return;
+    }
+  }
+  file.downloadCount++;
+  await file.save();
+  console.log(file.downloadCount);
+
+  res.download(file.path, file.originalName);
+}
+
+app.listen(process.env.PORT);
