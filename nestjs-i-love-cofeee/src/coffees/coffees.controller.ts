34d84f9502c13ee967851e27edac93791a07bf31
Omer Karakas <karakasomer@gmail.com>
2022-08-26 08:35:39 +0300

    new coffee service, dto, entity added

--- nestjs-i-love-cofeee/src/coffees/coffees.controller.ts
+++ nestjs-i-love-cofeee/src/coffees/coffees.controller.ts
@@ -1,19 +1,58 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll() {
-    return 'this returns all coffees';
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery) {
+    //const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() params) {
-    return `This action returns #${params.id} coffee(s)`;
+  findOne(@Param('id') id: number) {
+    //throw 'A random error';
+    console.log(typeof id);
+    const coffee = this.coffeesService.findOne(id + '');
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+
+      // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return coffee;
   }
 
   @Post()
-  create(@Body('name') body) {
-    return body;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
